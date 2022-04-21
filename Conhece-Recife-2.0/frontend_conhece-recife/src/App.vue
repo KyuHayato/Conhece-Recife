7a1bac5a930c421472f28c405d527a103aeb8694
KyuHayato <ewerson.alves@live.com>
2022-04-21 16:31:53 -0300

    Atualizações de versão e versão mobile em inicio de desenvolvimento

---
+++ Conhece-Recife-2.0/frontend_conhece-recife/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/Conhece Recife Logo.png">
+    <HelloWorld msg="Conhece Recife 2.0 Frontend"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
